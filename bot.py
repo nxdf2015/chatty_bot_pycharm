9945dd458d72f601fb52a0e98e9d5effbf8da6db
xavier niquet <niquet_xavier2002@yahoo.fr>
2021-01-02 10:23:00 +0100

    stage 2

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-name_bot = "xavier"
-print(f'Hello! My name is {name_bot}.')
+
+print(f'Hello! My name is Aid.')
 print('I was created in 2020.')
 
 print('Please, remind me your name.')
-print('What a great name you have, {your_name}!')
+name = input()
+print(f'What a great name you have, {name}!')
