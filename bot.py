8b73d0719218dce3c60204948eabba8ca99b4346
xavier niquet <niquet_xavier2002@yahoo.fr>
2021-01-02 10:20:25 +0100

    stage 1

---
+++ bot.py
@@ -0,0 +1,6 @@
+name_bot = "xavier"
+print(f'Hello! My name is {name_bot}.')
+print('I was created in 2020.')
+
+print('Please, remind me your name.')
+print('What a great name you have, {your_name}!')
