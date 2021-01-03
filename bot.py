d6700c67e881372cfcbe60088ed577700fcdf639
xavier niquet <niquet_xavier2002@yahoo.fr>
2021-01-03 09:05:26 +0100

    stage 4

--- bot.py
+++ bot.py
@@ -8,9 +8,17 @@ print('What a great name you have, ' + name + '!')
 print('Let me guess your age.')
 print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-remainder3 = int(input())
-remainder5 = int(input())
-remainder7= int(input())
-your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+rem3 = int(input())
+rem5 = int(input())
+rem7 = int(input())
 
-print(f"Your age is {your_age}; that's a good time to start programming!")
+age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+print('Now I will prove to you that I can count to any number you want.')
+
+number  = int(input())
+for i in range(number + 1):
+    print(f'{i} !')
+
+print('Completed, have a nice day!')
